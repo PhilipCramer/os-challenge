c363d288f7ddabd2df97fb6f8ef4a77a4d559ed3
Philip Cramer <s224319@dtu.dk>
2023-09-19 14:06:14 +0200

    changed server socket to bind to all interfaces

--- src/main.c
+++ src/main.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
+#include <unistd.h>
 
 
 int main(int argc, char *argv[]){
@@ -35,8 +37,8 @@ int main(int argc, char *argv[]){
 
     // Set port and IP:
     server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(2000);
-    server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    server_addr.sin_port = htons(port_num);
+    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
 
     // Bind to the set port and IP:
     if(bind(socket_desc, (struct sockaddr*)&server_addr, sizeof(server_addr))<0){
