983b4ae4d7068bc625d3aad2e259995499cfd523
Philip Cramer <s224319@dtu.dk>
2023-09-25 22:38:15 +0200

    created priority queue functionality

---
+++ src/priority_queue.h
@@ -0,0 +1,21 @@
+#include<stdlib.h>
+
+typedef struct {
+  void * element;
+  int priority;
+} qelement_t;
+
+typedef struct {
+  int size;
+  int last;
+  qelement_t *array;
+}prio_queue_t;
+
+int initialize_queue(prio_queue_t *reference); 
+int enqueue(prio_queue_t *reference, void *data, int priority);
+void bubbleUp(prio_queue_t *reference, int pos);
+void bubbleDown(prio_queue_t *reference, int pos);
+void *dequeue(prio_queue_t *reference);
+int destroy_queue(prio_queue_t * reference);
+void printQ(prio_queue_t *reference);
+
