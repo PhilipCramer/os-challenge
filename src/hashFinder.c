a7bdfb1653904af21ad6d6800c6ab4557b81e016
Philip Cramer <s224319@dtu.dk>
2023-09-25 16:04:09 +0200

    removed unnecessary print statements

--- src/hashFinder.c
+++ src/hashFinder.c
@@ -1,12 +1,11 @@
 #include "hashFinder.h"
 
-int64_t findHash(char hashToFind[], int64_t start, int64_t stop) {
-  char *foundHash;
+int64_t findHash(unsigned char hashToFind[], int64_t start, int64_t stop) {
+  unsigned char *foundHash;
   int64_t current;
   for(current = start; current < stop; current++){
     foundHash = SHA256((const unsigned char *) &current, sizeof(current), NULL);
-    if (strcmp(hashToFind, foundHash) == 0){
-      //free(foundHash);
+    if (memcmp(hashToFind, foundHash, SHA256_DIGEST_LENGTH) == 0){
       return current;
     }
   }
