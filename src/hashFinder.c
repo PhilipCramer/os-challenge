875c955f1b61ef2ceb85ca4f52773a1dd73d9f61
nts96 <s224317@student.dtu.dk>
2023-11-15 15:37:01 +0100

    Added sha256 buffer.

--- src/hashFinder.c
+++ src/hashFinder.c
@@ -3,8 +3,9 @@
 uint64_t find_hash(unsigned char hash_to_find[], uint64_t start, uint64_t stop) {
   unsigned char *found_hash;
   uint64_t current;
+  unsigned char buffer[32];
   for(current = start; current < stop; current++){
-    found_hash = SHA256((const unsigned char *) &current, sizeof(current), NULL);
+    found_hash = SHA256((const unsigned char *) &current, sizeof(current), buffer);
     if (memcmp(hash_to_find, found_hash, SHA256_DIGEST_LENGTH) == 0){
       return current;
     }
